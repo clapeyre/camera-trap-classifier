00748a7046a3492c545eec697db7cd97f13d0557
Nicolas Hermet <38986312+Nicolas-Hermet@users.noreply.github.com>
2022-01-07 15:23:57 +0100

    Fix async bug on results (#5)
    * Lint the code and add test cases to gitignore
    
    * Fix asynchronous default on picture rendering
    
    * Clean code
    
    * Fix console log wording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core'
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
 import { Prediction, RESULT, EMPTY_PREDICTION, DISPLAYED_COLUMNS } from './constants';
 
-import * as tf from '@tensorflow/tfjs'
+import * as tf from '@tensorflow/tfjs';
 
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -14,19 +14,16 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  title = 'angular-app'
+  title = 'angular-app';
 
-  filePath: string[] = [];
-  currentFileName: string;
-  fileNames: string[] = [];
-  images: HTMLImageElement[] = [];
+  blob: any[] = [];
+  filesQuantity: number = -1;
 
   loadingModel: Boolean = true;
   tableReady: Boolean = false;
-  model: tf.LayersModel
+  model: tf.LayersModel;
 
   result = RESULT;
-  order: Array<any>;
 
   predictResult: Uint8Array | Float32Array | Int32Array;
 
@@ -49,39 +46,45 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   async loadModel() {
-    this.model = await tf.loadLayersModel('/assets/model.json')
+    this.model = await tf.loadLayersModel('/assets/model.json');
     this.loadingModel = false;
   }
 
   loadInputFiles(e: Event) {
     const files = (e.target as HTMLInputElement).files!;
-    this.fileNames = [];
-    this.filePath = [];
-    this.images = [];
+    this.filesQuantity = files.length
+    // console.log({files});
+    this.blob = [];
 
     Object.keys(files).forEach((key, index) => {
-      this.fileNames.push(files[index].name);
-    const reader = new FileReader()
-    reader.onload = () => {
-        this.filePath.push( reader.result as string);
-        this.images.push(new Image());
-    }
-      reader.readAsDataURL(files[index])
-    })
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.blob.push({
+          image: new Image(),
+          name: files[index].name,
+          path: reader.result as String
+        });
+        // console.log('Blob loading', index, this.blob);
+      };
+      reader.readAsDataURL(files[index]);
+    });
+    console.log('After selection', {qnt: this.filesQuantity, loaded: this.blob.length});
   }
 
-  predictionLoop() {
-    this.filePath.forEach((file, index) => {
-      this.images[index].src = file;
+  predictionLoop() {;
+    console.log('At start of prediction', {qnt: this.filesQuantity, loaded: this.blob.length});
+    this.blob.forEach((object) => {
+      // console.log('Blob forEach', object);
+      object.image.onload = () => {
+        // console.log('from image onload', object);
+        this.predict(object.image, object.name);
+      }
+      object.image.src = object.path;
     })
-    this.images.forEach((x, index) => {
-      x.onload = () => {
-        this.predict(x, this.fileNames[index]);
-      };
-    });
   }
 
   async predict(image: HTMLImageElement, fileName: string) {
+    // console.log('from predict', image, fileName)
     let pre_image = tf.browser.fromPixels(image, 3)
       .resizeNearestNeighbor([224, 224])
       .expandDims()
@@ -89,25 +92,27 @@ export class AppComponent implements OnInit, AfterViewInit {
       .div(255)
       .reverse(-1);
     let predictResult = await (this.model.predict(pre_image) as tf.Tensor).data();
-    console.table({predictResult});
+    console.groupCollapsed(fileName);
+      console.table({predictResult});
+    console.groupEnd();
+
     this.predictions = Array.from(predictResult)
       .map( (p, i) => {
         return {
           probability: parseFloat(p.toFixed(3)),
           className: this.result[i]
-        }
-    }).sort(function (a, b) {
-			return b.probability - a.probability;
-    }).map((p, i) => {
-      return {
-        probability: i > 5 ? 0 : p.probability,
-        className: p.className
-      }
-    })
-    .reduce((map: {[key: string]: number}, obj: {probability: number, className: string}) => {
-      map[obj.className.toLowerCase()] = obj.probability;
-      return map;
-    }, {})
+        };
+      }).sort(function (a, b) {
+        return b.probability - a.probability;
+      }).map((p, i) => {
+        return {
+          probability: i > 5 ? 0 : p.probability,
+          className: p.className
+        };
+      }).reduce((map: {[key: string]: number}, obj: {probability: number, className: string}) => {
+        map[obj.className.toLowerCase()] = obj.probability;
+        return map;
+      }, {});
 
     this.predictions['fileName'] = fileName;
     this.predictions['preview'] = this.sanitizer.bypassSecurityTrustUrl(image.src);
